75db7905486e0b686e8be9fc5bdcc37ad8064f1c
Hemant Negi <hemant.negi@paytm.com>
2022-11-22 21:03:26 +0530

    use block instead of text in slack webhook

--- server/controllers/slack.controller.js
+++ server/controllers/slack.controller.js
@@ -3,20 +3,104 @@ import cookie from 'cookie';
 import lodash from 'lodash';
 import { MissingFieldError, InternalError } from '../errors';
 
-const formatMessage = (members, url, org, boardCode) => {
-  const lines = members.map(
-    ({ name, local_score, stars }) =>
-      `*${name}* - ${local_score} :points: Points, ${stars} :star: Stars`
-  );
 
-  const paragraphs = [
-    `Advent of Code Leaderboard :christmas_tree: ${org ? '| ' + org : ''} `,
-    `<${url}|Join the board> - _*${boardCode}*_`,
-    lines.join('\n'),
-    `<${url}|View online leaderboard>`,
-  ];
+const ICONS = {
+  FIRST_POSITION: 'https://cdn-icons-png.flaticon.com/512/3975/3975625.png',
+  SECOND_POSITION: 'https://cdn-icons-png.flaticon.com/512/3975/3975628.png',
+  THIRD_POSITION: 'https://cdn-icons-png.flaticon.com/512/3975/3975631.png',
+};
 
-  return paragraphs.join('\n\n');
+const getStars = (count) => {
+  if (!count) return '';
+
+  return [...new Array(count)].reduce((acc, curr) => {
+    return `${acc}:start: `;
+  }, '');
+};
+
+const getMedal = (index) => {
+  if (index > 2) return {};
+  let url = '';
+  switch (index) {
+    case 0:
+      url = ICONS.FIRST_POSITION;
+      break;
+    case 1:
+      url = ICONS.SECOND_POSITION;
+      break;
+    case 2:
+      url = ICONS.THIRD_POSITION;
+  }
+
+  return {
+    accessory: {
+      type: 'image',
+      image_url: url,
+      alt_text: `${index + 1} position`,
+    },
+  };
+};
+const formatDate = (date) => {
+  if (!date) return '';
+  return new Date(date * 1000).toLocaleDateString();
+};
+
+const divider = {
+  type: 'divider',
+};
+const createBlocks = (members, url, org, boardCode) => {
+  const blocks = [];
+  blocks.push({
+    type: 'section',
+    text: {
+      type: 'plain_text',
+      text: `${org ? org : ''} :christmas_tree: Advent of Code Leaderboard`,
+      emoji: true,
+    },
+  });
+  blocks.push({
+    type: 'context',
+    elements: [
+      {
+        type: 'image',
+        image_url: 'https://adventofcode.com/favicon.png',
+        alt_text: 'advent of code',
+      },
+      {
+        type: 'mrkdwn',
+        text: `<${url}|*Join the Paytm board*> ${boardCode}*`,
+      },
+    ],
+  });
+
+  blocks.push(divider);
+
+  members.forEach(({ name, stars, last_star_ts }, index) => {
+    const lastStarDate = formatDate(last_star_ts);
+    const starStr = getStars(stars);
+    blocks.push({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*${name}*\n *0*  :points: ${starStr ? '\n ' + starStr : ''}${
+          lastStarDate ? '\n *Last start won* -' + lastStarDate : ''
+        }`,
+      },
+      ...(getMedal(index)),
+    });
+
+    blocks.push(divider);
+  });
+
+  blocks.push({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `:arrow_right: <${url}|*View online leaderboard*> :arrow_left:`,
+    },
+  });
+
+  return blocks;
 };
 
 export const adventOfCode = async (req, res) => {
@@ -57,16 +141,18 @@ export const adventOfCode = async (req, res) => {
       '-local_score',
       '-stars',
     ]);
-    const message = formatMessage(members, url, ORGANISATION, BOARD_CODE);
 
+    // https://app.slack.com/block-kit-builder/T026NT2D4
+    const blocks = createBlocks(members, url, ORGANISATION, BOARD_CODE);
     await axios.post(WEBHOOK_URI, {
       username: `Advent of Code Leaderboard ${
         ORGANISATION ? '| ' + ORGANISATION : ''
       } `,
       icon_emoji: ':christmas_tree:',
-      text: message,
+      // text: message,
+      blocks,
     });
-    return res.status(200).send({ response: { message } });
+    return res.status(200).send({ response: "ok" });
   } catch (_err) {
     // err contains sensitive info
     throw new InternalError();
