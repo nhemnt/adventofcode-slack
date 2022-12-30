504bd1beafafd18d67e8335f573cec8bf2b44d6e
Hemant Negi <hemant.negi@paytm.com>
2022-12-30 10:32:44 +0530

    upate

--- server/controllers/test.controller.js
+++ server/controllers/test.controller.js
@@ -2,29 +2,21 @@ import axios from 'axios';
 import { MissingFieldError, InternalError } from '../errors';
 
 export const test = async (req, res) => {
-  const { TEST_WEBHOOK_URI } = process.env;
+  const { QA_URL, QA_SECRET } = process.env;
 
-  if (TEST_WEBHOOK_URI === null) {
+  if (QA_URL === null) {
+    throw new MissingFieldError('Missing webhook URI');
+  }
+  if (QA_SECRET === null) {
     throw new MissingFieldError('Missing webhook URI');
   }
 
   try {
     // https://app.slack.com/block-kit-builder/T026NT2D4
-    await axios.post(TEST_WEBHOOK_URI, {
-      username: 'Slackify',
-      blocks:  [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Test',
-            },
-          },
-        ]
-    });
+    await axios.post(QA_URL, {}, { headers: { Authorization: QA_SECRET }});
     return res.status(200).send({ response: 'ok' });
   } catch (_err) {
     // err contains sensitive info
-    throw new InternalError(_err);
+    throw new InternalError();
   }
 };
